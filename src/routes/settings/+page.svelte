773b827255930f8f896f0658277a6719990e449a
kinnnever <thienbinh192837@gmail.com>
2025-06-04 15:15:51 +0700

    Update +page.svelte
    Signed-off-by: kinnnever <thienbinh192837@gmail.com>

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,14 +2,7 @@
 	import Title from '$components/Title.svelte';
 	import Icon from '@iconify/svelte';
 
-	let members = [
-		{ name: 'Lưu Thế Kiên', role: 'Trưởng nhóm', mission: 'Code chính, Nội dung' },
-		{ name: 'Thùng Việt Hoàn', role: 'Thành viên', mission: 'Thuyết trình, PowerPoint phụ' },
-		{ name: 'Phạm Thành Long', role: 'Thành viên', mission: 'Word chính, PowerPoint chính' },
-		{ name: 'Nguyễn Thành Trung', role: 'Thành viên', mission: 'Word phụ, Nội dung' },
-		{ name: 'Dương Tuấn Huy', role: 'Thành viên', mission: 'Word phụ' },
-		{ name: 'Đỗ Trọng Giáp', role: 'Thành viên', mission: 'PowerPoint phụ, Nội dung' },
-	];
+	
 </script>
 
 <svelte:head>
