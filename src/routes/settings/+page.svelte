cb46acc910c67d9bc2b1dfcd345332733363a45a
kinnnever <thienbinh192837@gmail.com>
2025-06-04 15:16:45 +0700

    Update +page.svelte
    Signed-off-by: kinnnever <thienbinh192837@gmail.com>

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -8,27 +8,3 @@
 <svelte:head>
 	<title>Pom.moe - Nhóm</title>
 </svelte:head>
-<h1 class="mb-6 text-2xl font-bold text-white">DANH SÁCH THÀNH VIÊN</h1>
-
-<div class="overflow-x-auto rounded-md bg-black bg-opacity-30 p-4 ring-1 ring-white/5">
-	<table class="w-full table-auto border-collapse text-white">
-		<thead class="bg-white/10 text-left">
-			<tr>
-				<th class="p-2">STT</th>
-				<th class="p-2">Họ và Tên</th>
-				<th class="p-2">Vai trò</th>
-				<th class="p-2">Nhiệm vụ</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each members as member, i}
-				<tr class="odd:bg-white/5 even:bg-white/0">
-					<td class="p-2">{i + 1}</td>
-					<td class="p-2">{member.name}</td>
-					<td class="p-2">{member.role}</td>
-					<td class="p-2">{member.mission}</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-</div>
